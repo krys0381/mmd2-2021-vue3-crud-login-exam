7a106f7a86811151ad6310f7b14a3fc6d0a4813b
Mary Rose Avenido <maryrose_avenido@yahoo.com>
2022-05-27 17:33:51 +0200

    fix on hero

--- src/views/HomepageView.vue
+++ src/views/HomepageView.vue
@@ -1,69 +1,60 @@
 <template>
-    <div class="homepage ">
+    <div class="homepage-out-container">
         <Header />
-        <div class="homepage-row container-fluid d-flex align-items-end overflow-hidden">
-            <div class="container">
-                <div class="row" id="homepage-container">
+        <div class="homepage-row container-fluid d-flex align-items-end justify-content-center overflow-hidden">
+            <div class="container d-flex align-items-center justify-content-center">
+                <div class="row" id="homepage-container" >
+                    <div class="hero-content-container d-flex flex-column align-items-center col-lg-12 col-sm-12 col-12  justify-content-center">
+                        <h1>Because starting up a tech business should be possible.</h1>
+                        <h3 class="col-lg-8">
+                            We are a community that connects, inspires and helps start-ups, students and investors build their tech business through networking events.
+                        </h3>
+                          <button type="button" class="btn btn-primary btn-lg 2 col-lg-4 col-md-12 col-sm-12 col-12 mt-5">JOIN OUR EVENT NOW!</button>
+                    </div>
+
+
                     <!-- <div class="col-12 p-0"> -->
                         <!-- <div id="homepage-container"> -->
 
-                        <div class="hero-text-container d-flex flex-column align-items-center justify-content-center">
-                             <div class="hero-container">
-                                <div id="hero-heading">
-                                    <h1>Because starting up a tech business should be possible.</h1>
-                                </div>
-                            </div>
-
-                            <div class="hero-container">
-                                <div id="hero-text">
-                                    <h3>We are a community that connects, inspires and helps start-ups, students and investors build their tech business through networking events.</h3>
-                                </div>
-                            </div>
-                            <button type="button" class="btn btn-primary btn-lg 2 col-lg-4 col-md-12 col-sm-12 col-12 mt-5">JOIN OUR EVENT NOW!</button>
-                            <!-- <div class="hero-container col-lg-12 col-sm-12 col-12">
-                                <div id="hero-btn">
-                                    <button type="button" class="btn btn-primary btn-lg 2">JOIN OUR EVENT NOW!</button>
-                                </div>
-                            </div> -->
-                            <!-- <div class="hero-image-container d-none d-xl-flex">
-
-                            </div> -->
-                        </div>
-
-                            <div class="hero-container d-none d-xl-flex">
-                                <div id="link-btns">
-                                    <a href="" class="link-btn d-flex align-items-center">
-                                   
-                                        <div class="link-btn-text">
-                                            <div class="link-btn-text-small">LOOKING FOR FUNDING?</div>
-                                            <div class="link-btn-text-big mt-4">Start-ups</div>
-                                        </div>
-                                        <div class="link-btn-arrow">
-                                            <!-- <img src="../assets/img/angle-right-solid.svg" alt=""> -->
-                                        </div>
-                                    </a>
-                                    
-
-                                    <a href="" class="link-btn d-flex align-items-center">
-                                        <div class="link-btn-text">
-                                            <div class="link-btn-text-small">READY TO INVEST IN START-UPS?</div>
-                                            <div class="link-btn-text-big mt-4">Investors</div>
-                                        </div>
-                                        <div class="link-btn-arrow">
-                                            <!-- <img src="../assets/img/angle-right-solid.svg" alt=""> -->
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
 
                         <!-- </div> -->
                     <!-- </div> -->
                 </div>
             </div>
-               <div class="hero-image-container d-none d-xl-flex">
+            <div class="hero-image-container d-none d-xl-flex">
 
-                </div>
+            </div>
         </div>
+        <div class="container-fluid "> 
+            <div class="container ">
+                <div class="row d-flex align-items-center justify-content-center">
+                    <div class="hero-container d-none d-xl-flex align-items-center justify-content-center">
+                        <div id="link-btns">
+                            <a href="" class="link-btn d-flex align-items-center">
+                            
+                                <div class="link-btn-text">
+                                    <div class="link-btn-text-small">LOOKING FOR FUNDING?</div>
+                                    <div class="link-btn-text-big mt-4">Start-ups</div>
+                                </div>
+                                <div class="link-btn-arrow">
+                                    <!-- <img src="../assets/img/angle-right-solid.svg" alt=""> -->
+                                </div>
+                            </a>
+                            <a href="" class="link-btn d-flex align-items-center">
+                                <div class="link-btn-text">
+                                    <div class="link-btn-text-small">READY TO INVEST IN START-UPS?</div>
+                                    <div class="link-btn-text-big mt-4">Investors</div>
+                                </div>
+                                <div class="link-btn-arrow">
+                                    <!-- <img src="../assets/img/angle-right-solid.svg" alt=""> -->
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+       
         <IntroductionComponent />
         <OurServicesComponent />
         <EventsPreviewComponent />
@@ -73,7 +64,8 @@
         <NewsletterComponent />
         <ConclusionComponent />
         <Footer />
-    </div>
+</div>
+   
 </template>
 
 <script>
@@ -115,75 +107,39 @@ export default {
 a {
     text-decoration: none;
 }
+.homepage-out-container{
+    position: relative;
+}
 
-.homepage-row {
-    // width: 100%;
-    // height: 100%
-    width: 100vw;
-    min-height: 90vh;
-    background: transparent linear-gradient(180deg, #7E42BE 0%, #10041C 100%) 0% 0% no-repeat padding-box;
-    // background-image: url("../assets/img/hero_homepage.svg");
-    // background-size: cover;
-    // background-repeat: no-repeat;
-    // background-position: center;
-
-    .hero-image-container{
-        background-image: url('../assets/img/hero_transparent-04-01.svg');
+ .hero-image-container{
+        background-image: url('../assets/img/hero_transparent-5-01.svg');
         background-size: cover;
         background-position: center;
         width: 100vw;
-        height: 60%;
-        margin-left: -12px;
+        height: 70%;
+        // margin-left: -12px;
         position: absolute;
         // margin-top: 200px;
-        // margin-bottom: 0px
+    //   margin-bottom: 100px;
         
     }
 
-    .btn {
-        background-color: #E69B5A;
-        border-radius: 0;
-        border-color: #E69B5A;
-        padding: 15px 30px;
-        z-index: 1;
-        transition: 0.3s;
+.btn {
+    background-color: #E69B5A;
+    border-radius: 0;
+    border-color: #E69B5A;
+    padding: 15px 30px;
+    z-index: 1;
+    transition: 0.3s;
 
-        &:hover{
-            background-color: $accentdarkcolor;
-        }
+    &:hover{
+        background-color: $accentdarkcolor;
     }
+}
 
-    #homepage-container {
-        width: 100%;
-        height: 100%;
-        padding: 200px 0 300px 0;
-
-        .hero-container {
-            display: flex;
-            justify-content: center;
-            z-index: 1;
-
-            #hero-heading {
-                width: 100%;
-                height: 25%;
-                margin-bottom: 20px;
-            }
-
-            #hero-text {
-                width: 60%;
-                height: 25%;
-                // margin-top: 10px;
-            }
-
-            #hero-btn {
-                margin-top: 40px;
-
-                
-            }
-
-            #link-btns {
+  #link-btns {
                 width: 80%;
-                margin-top: 240px;
+                margin-top: 0px;
                 display: flex;
                 position: absolute;
                 height: 184px;
@@ -249,6 +205,51 @@ a {
                         }
                     }
                 }
+            }
+.homepage-row {
+    // width: 100%;
+    // height: 100%
+    position: relative;
+    width: 100vw;
+    height: 90vh;
+    background: transparent linear-gradient(180deg, #7E42BE 0%, #10041C 100%) 0% 0% no-repeat padding-box;
+    // background-image: url("../assets/img/hero_homepage.svg");
+    // background-size: cover;
+    // background-repeat: no-repeat;
+    // background-position: center;
+
+   
+
+    .hero-content-container{
+         z-index: 1;
+    }
+
+    #homepage-container {
+        width: 100vw;
+        height: 100vh;
+        // padding: 200px 0 300px 0;
+
+        .hero-container {
+            display: flex;
+            justify-content: center;
+            z-index: 1;
+
+            #hero-heading {
+                width: 100%;
+                height: 25%;
+                margin-bottom: 20px;
+            }
+
+            #hero-text {
+                width: 60%;
+                height: 25%;
+                // margin-top: 10px;
+            }
+
+            #hero-btn {
+                margin-top: 40px;
+
+                
             }
         }
     }  
